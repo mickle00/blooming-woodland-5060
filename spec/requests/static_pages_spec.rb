1ba93255664412f169593d9ad611a056dde6f627
Michael Stewart <mickle00@gmail.com>
2012-11-26 18:32:06 -0800

    Initial dynamic values

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,11 @@ describe "StaticPages" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                                :text => "Ruby on Rails | Home")
+    end
   end
 
   describe "Help page" do
