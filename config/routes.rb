5667770cefafe4bd2b6094fa6693c4451ede729b
Michael Stewart <mickle00@gmail.com>
2012-11-29 21:05:09 -0800

    Adding signin, signout, and cookies

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,12 @@ RailsApp::Application.routes.draw do
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/signup',   to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   resources :microposts
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
